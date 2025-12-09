c1163d16a4febe6857a1e8655d44dbafa869e0b1
OhtoGangg <ogealaviiva@gmail.com>
2025-12-09 15:13:58 +0200

    Update ping.js

--- Commands/Public/ping.js
+++ Commands/Public/ping.js
@@ -1,48 +1,17 @@
-const { ChatInputCommandInteraction,
-    SlashCommandBuilder,
-    PermissionFlagsBits,
-    Client} = require("discord.js")
-const { loadCommands } = require("../../Handlers/commandHandler")
-const { loadEvents } = require("../../Handlers/eventHandler")
+const { SlashCommandBuilder, ChatInputCommandInteraction, InteractionResponseFlags } = require("discord.js")
+
 module.exports = {
-    data: new SlashCommandBuilder()
-    .setName("reload")
-    .setDescription("Käynnistää komennot/tapahtumat uudestaan.")
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
-    .setDMPermission(false)
-    .addStringOption((options) => options
-    .setName("kategoria")
-    .setDescription("Valitse haluatko käynnistää komennot vai tapahtumat uudelleen.")
-    .setRequired(true)
-    .addChoices(
-        { name: "Tapahtumat", value: "events"},
-        { name: "Komennot", value: "commands"}
-    )),
-    /**
-     * 
-     * @param {ChatInputCommandInteraction} interaction 
-     * @param {Client} client 
-     */
-    async execute(interaction, client) {
-        const subCommand = interaction.options.getString("kategoria")
-        
-        switch(subCommand) {
-            case "events" : {
-                for (const [key, value] of client.events)
-                client.removeAllListeners(`${key}`, value, true)
-                loadEvents(client)
-                interaction.reply({content: "Tapahtumat käynnistetty uudelleen", ephemeral: true})
-            }
-            break
-            case "commands" : {
-                await loadCommands(client)
-                try {
-                    interaction.reply({content: "Komennot käynnistetty uudelleen", ephemeral: true})
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-            break
-        }
-    }
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Vastaa Pong!"),
+
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  async execute(interaction) {
+    await interaction.reply({ 
+      content: "Pong!", 
+      flags: InteractionResponseFlags.Ephemeral 
+    })
+  },
 }
